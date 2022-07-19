43dd2f495519a86df3009937ec2bb7e677cb6bc9
Meresia13 <melisaopiyo@gmail.com>
2022-07-19 01:47:12 -0700

    hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,18 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int a;
+
+	while(1)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
+	a = *s++;
+	if (a == c)
+	{
+	(return (s -1);
 	}
-	return (!c ? s : NULL);
+	if (a == 0)
+	{
+	return (NULL);
+	}
+	}
 }
