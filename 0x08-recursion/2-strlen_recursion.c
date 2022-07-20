27d48142b19648c356524672e3bd1e1dc36044a6
Meresia13 <melisaopiyo@gmail.com>
2022-07-20 13:09:27 -0700

    hot fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,16 +3,11 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: returns the length of a string
- * Return: nothing
+ * Return: length of string
  */
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0', ++i)
-
-	{
-		_strlen_recursion(s[i]);
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
